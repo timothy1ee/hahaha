d9c8babeb58a65489de3edd1cda6576c41c0982a
Timothy Lee <tim@codepath.com>
2016-10-27 11:56:57 -0700

    Initial Commit

---
+++ hahaha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hahaha
+//
+//  Created by Timothy Lee on 10/27/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
